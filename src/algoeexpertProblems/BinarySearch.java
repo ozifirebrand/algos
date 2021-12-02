3b85285c8afa6ad77ceaf2125a3c65199121e050
Ozioma <oziomaokoroafor@gmail.com>
2021-12-02 09:28:15 +0100

    Binary search commit

--- src/algoeexpertProblems/BinarySearch.java
+++ src/algoeexpertProblems/BinarySearch.java
@@ -1,5 +1,13 @@
 package algoeexpertProblems;
 
 public class BinarySearch {
-
+    public static void findMinAndMax(int [] values, int aValue){
+        int maximum= values.length;
+        int minimum=0;
+        for (int number: values){
+            if ( maximum<minimum ){
+                break;
+            }
+        }
+    }
 }
