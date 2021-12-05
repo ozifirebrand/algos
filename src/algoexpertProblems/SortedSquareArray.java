cf2854cb79f554ef6a6ee65c73e8722beb439757
Ozioma <oziomaokoroafor@gmail.com>
2021-12-05 21:09:31 +0100

    Writing a sorted square program with the O(n) time and O(n) space time-space complexity - more efficient

--- src/algoexpertProblems/SortedSquareArray.java
+++ src/algoexpertProblems/SortedSquareArray.java
@@ -15,9 +15,31 @@ public class SortedSquareArray {
         return squaredArray;
     }
 
+    public static int[] sortAndSquare2(int [] anArray){
+        int smallestValueIndex =0;
+        int largestValueIndex = anArray.length-1;
+        int [] sortedArray = new int[anArray.length];
+        for (int index = anArray.length-1; index>=0; index--){
+            int smallestValue = anArray[smallestValueIndex];
+            int largestValue = anArray[largestValueIndex];
+            //this is because we assume that the array 'anArray' is sorted
+
+            if ( Math.abs(smallestValue)> Math.abs(largestValue)) {
+                sortedArray[index] = smallestValue * smallestValue;
+                smallestValueIndex++;
+            }
+            else {
+                sortedArray[index] = largestValue* largestValue;
+                largestValueIndex--;
+            }
+        }
+        return sortedArray;
+    }
+
     public static void main(String[] args) {
         int [] array = {-2, -1};
 
         System.out.println(Arrays.toString(sortAndSquare(array)));
+        System.out.println(Arrays.toString(sortAndSquare2(array)));
     }
 }
