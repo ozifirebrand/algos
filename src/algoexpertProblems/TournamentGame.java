44b412520901ff01f335ed90d11a88f6cc75b3e9
Ozioma <oziomaokoroafor@gmail.com>
2021-12-09 10:14:45 +0100

    Tournament game incomplete

---
+++ src/algoexpertProblems/TournamentGame.java
@@ -0,0 +1,12 @@
+package algoexpertProblems;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TournamentGame {
+//    public static void playGame(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results){
+//        Map<String, Integer> compMap = new HashMap<>();
+//        for ()
+//    }
+}
