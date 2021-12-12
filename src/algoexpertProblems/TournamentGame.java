e02e2bef11b1cc1a703c285c73beb8450c220456
Ozioma <oziomaokoroafor@gmail.com>
2021-12-12 23:48:56 +0100

    Tournament game trial

--- src/algoexpertProblems/TournamentGame.java
+++ src/algoexpertProblems/TournamentGame.java
@@ -5,8 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TournamentGame {
-//    public static void playGame(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results){
-//        Map<String, Integer> compMap = new HashMap<>();
-//        for ()
-//    }
+    public static void playGame(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results){
+        Map<String, Integer> compMap = new HashMap<>();
+
+
+    }
 }
