35610d012f12a4a616dda9bb540e2b4f22a839b7
Ozioma <oziomaokoroafor@gmail.com>
2021-12-18 23:58:58 +0100

    It is well"

---
+++ src/algoexpertProblems/MinimumChange.java
@@ -0,0 +1,9 @@
+package algoexpertProblems;
+
+public class MinimumChange {
+    public int nonConstructibleChange(int[] coins) {
+//      It is well oo
+
+        return -1;
+    }
+}
--- src/algoexpertProblems/TournamentGame.java
+++ src/algoexpertProblems/TournamentGame.java
@@ -8,9 +8,9 @@ Go through the results table and update each team's score
 Find the highest scoring team and return
  */
 public class TournamentGame {
-    public static String playGame(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results){
+    public static String playGame(ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
         int points;
-        int maxValue=0;
+        int maxValue = 0;
         String maxKey = "";
         int awayPlayerIndex = 1;
         int homePlayerIndex = 0;
@@ -19,47 +19,34 @@ public class TournamentGame {
 
         Map<String, Integer> playersMap = new HashMap<>();
 
-        for (int index = 0; index< competitions.size(); index++){
+        for (int index = 0; index < competitions.size(); index++) {
 
             int scorePerRound = results.get(index);
-            ArrayList <String> playersPerRound = competitions.get(index);
+            ArrayList<String> playersPerRound = competitions.get(index);
 
-            if ( scorePerRound == 0 ){
+            if ( scorePerRound == 0 ) {
                 String player = playersPerRound.get(awayPlayerIndex);
-                if ( playersMap.containsKey(player) ){
-                    points= playersMap.get(player);
-                    playersMap.put(player, points+additionalPoint);
-                }else playersMap.put(player, additionalPoint);
+                if ( playersMap.containsKey(player) ) {
+                    points = playersMap.get(player);
+                    playersMap.put(player, points + additionalPoint);
+                } else playersMap.put(player, additionalPoint);
             }
 
-            if ( scorePerRound == 1 ){
+            if ( scorePerRound == 1 ) {
                 String player = playersPerRound.get(homePlayerIndex);
-                if ( playersMap.containsKey(player) ){
-                    points= playersMap.get(player);
-                    playersMap.put(player, points+additionalPoint);
-                }else playersMap.put(player, additionalPoint);
+                if ( playersMap.containsKey(player) ) {
+                    points = playersMap.get(player);
+                    playersMap.put(player, points + additionalPoint);
+                } else playersMap.put(player, additionalPoint);
             }
         }
 
-        for (String key : playersMap.keySet()){
-            if ( playersMap.get(key) > maxValue ){
+        for (String key : playersMap.keySet()) {
+            if ( playersMap.get(key) > maxValue ) {
                 maxValue = playersMap.get(key);
-                maxKey= key;
+                maxKey = key;
             }
         }
         return maxKey;
     }
-
-//    public static String getWinner(Map<String, Integer> playersMap){
-//        int maxValue=0;
-//        String maxKey = "";
-//        for (String key : playersMap.keySet()){
-//            if ( playersMap.get(key) > maxValue ){
-//                maxValue = playersMap.get(key);
-//                maxKey= key;
-//            }
-//        }
-//        return maxKey;
-//
-//    }
 }
