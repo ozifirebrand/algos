81a81cf01d9f61cd3eaae4ea58e955e69d2dd9ce
Ozioma <oziomaokoroafor@gmail.com>
2021-12-26 02:14:21 +0100

    Minimum non-construcrible change first working commit

--- src/algoexpertProblems/MinimumChange.java
+++ src/algoexpertProblems/MinimumChange.java
@@ -1,9 +1,26 @@
 package algoexpertProblems;
 
-public class MinimumChange {
-    public int nonConstructibleChange(int[] coins) {
-//      It is well Don't know what to writw
+import java.util.Arrays;
 
-        return -1;
+public class MinimumChange {
+    public static int nonConstructibleChange(int[] coins) {
+        Arrays.sort(coins);
+        int possibleChange = 0;
+
+
+
+        for (int coin : coins){
+            if ( coin > possibleChange + 1 ) return possibleChange+1;
+            else if ( coin <= possibleChange +1 ){
+                possibleChange += coin;
+            }
+        }
+
+        return possibleChange;
+    }
+
+    public static void main(String[] args) {
+        int [] coins = { 1, 1, 4};
+        System.out.println(nonConstructibleChange(coins));
     }
 }
