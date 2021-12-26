2b56be5ba47aa08a6850160322f2ceeffbae424c
Ozioma <oziomaokoroafor@gmail.com>
2021-12-26 02:29:56 +0100

    refactored commit

--- src/algoexpertProblems/MinimumChange.java
+++ src/algoexpertProblems/MinimumChange.java
@@ -10,17 +10,21 @@ public class MinimumChange {
 
 
         for (int coin : coins){
-            if ( coin > possibleChange + 1 ) return possibleChange+1;
+            if ( coin > possibleChange + 1 ) return possibleChange + 1;
             else if ( coin <= possibleChange +1 ){
                 possibleChange += coin;
             }
         }
 
-        return possibleChange;
+        return possibleChange + 1;
     }
 
     public static void main(String[] args) {
-        int [] coins = { 1, 1, 4};
+        int [] coins = { 5, 7, 1, 1, 2, 3, 22};
+        int [] coins1 = { 1, 1, 1, 1, 1};
+        int [] coins2 = {1, 5, 1, 1, 1, 10, 15, 20, 100};
         System.out.println(nonConstructibleChange(coins));
+        System.out.println(nonConstructibleChange(coins1));
+        System.out.println(nonConstructibleChange(coins2));
     }
 }
