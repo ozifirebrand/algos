8014a71b753338479a14352ec17cb9ed3937cbca
Ozioma <oziomaokoroafor@gmail.com>
2022-04-17 08:41:00 +0100

    Exponential and reverse

--- src/algoexpertProblems/UsingRecursion.java
+++ src/algoexpertProblems/UsingRecursion.java
@@ -1,6 +1,8 @@
 package algoexpertProblems;
 
 
+import java.util.Arrays;
+
 public class UsingRecursion {
     public static int factorial(int n){
         if ( n < 2 ) return 1;
@@ -14,9 +16,33 @@ public class UsingRecursion {
        if ( n ==1  ) return newX;
        return newX * exponential(x, n -1);
     }
+    public static int exponentialIteration(int x, int n){
+        int newX = 1;
+        for ( int i = n; i >= 1; i --){
+            newX *= x;
+        }
+        return newX;
+    }
+
+    public static int[] reverseStack(int[] array, int first, int last){
+        if ( first == last ){
+            return array;
+        }
+        int saved = array[first];
+        array[first] = array[last];
+        array[last] = saved;
+        first ++;
+        last --;
+        reverseStack(array, first, last);
+        return array;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(factorial(5));
         System.out.println(exponential(2, 13));
+        System.out.println(exponentialIteration(12, 13));
+        int [] normalArray = new int[]{4, 2, 0, 4, 1, 8, 9};
+        System.out.println(Arrays.toString(reverseStack(normalArray, 0, normalArray.length-1)));
     }
 }
