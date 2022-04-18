ee170b49f15bf4d6b05e4759206caab12c62f241
Ozioma <oziomaokoroafor@gmail.com>
2022-04-18 12:27:48 +0100

    Tower of hanoi unsoleved

--- src/algoexpertProblems/UsingRecursion.java
+++ src/algoexpertProblems/UsingRecursion.java
@@ -1,7 +1,7 @@
 package algoexpertProblems;
-
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class UsingRecursion {
     public static int factorial(int n){
@@ -46,7 +46,6 @@ public class UsingRecursion {
     }
 
     public static int maximumElement(int [] array, int index, int currentMaxNumber){
-         int maxElement = Integer.MIN_VALUE;
         if ( index > array.length-1 ) return currentMaxNumber;
         if ( array[index] > currentMaxNumber ){
             currentMaxNumber = array[index];
@@ -55,15 +54,49 @@ public class UsingRecursion {
         return maximumElement(array, index, currentMaxNumber);
     }
 
+    public static List<Integer> towerOfHanoi
+            (int index, List<Integer> source, List<Integer> spare, List<Integer> dest) {
+        /*
+        transfer top of source to spare
+        remove transferred element from source
+        transfer other parts to spare and delete them from source
+        if source contains no element, remove elements from spare
+        transfer elements from spare to dest
+        remove transferred elements from spare
+        if source or spare remain one element, send it to dest
+        if source and spare are empty, return dest
+        */
+        return towerOfHanoi( index, source, spare, dest );
+    }
+
+
 
     public static void main(String[] args) {
         int maxElement = Integer.MIN_VALUE;
-        System.out.println(factorial(5));
-        System.out.println(exponential(2, 13));
-        System.out.println(exponentialIteration(12, 13));
+        System.out.println("Factorial of 5 is: "+factorial(5));
+        System.out.println();
+        System.out.println("Using recursion to find exponential of 2^13 is: "+exponential(2, 13));
+        System.out.println();
+        System.out.println("Using for loop to find exponential of 12^13 is: "+exponentialIteration(12, 13));
         int [] normalArray = new int[]{4, 222, 2, 0, 4, 22, 14, 1, 8, 9};
-        System.out.println(Arrays.toString(reverseStack(normalArray, 0, normalArray.length-1)));
-        System.out.println(sumElement(normalArray, 0));
-        System.out.println(maximumElement(normalArray, 0, maxElement));
+        System.out.println();
+        System.out.println("Reversed form of "+ Arrays.toString(normalArray) +" is: "+Arrays.toString(reverseStack(normalArray, 0, normalArray.length-1)));
+        System.out.println();
+        System.out.println("Sum of elements in "+ Arrays.toString(normalArray) +" is: "+sumElement(normalArray, 0));
+        System.out.println();
+        System.out.println("Maximum element in "+ Arrays.toString(normalArray) +" is: "+maximumElement(normalArray, 0, maxElement));
+        System.out.println();
+        List<Integer> source = new ArrayList<>();
+        source.add(4);
+        source.add(5);
+        source.add(8);
+        source.add(14);
+        source.add(31);
+        source.add(32);
+        List<Integer> spare = new ArrayList<>();
+        List<Integer> dest = new ArrayList<>();
+        int index = source.size() -1;
+        System.out.print("Stacking the tower of numbers: " +source);
+        System.out.println("\nInto "+ towerOfHanoi(index, source, spare, dest));
     }
 }
