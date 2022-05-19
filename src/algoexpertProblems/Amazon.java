10d461d046574ec72dfe72c04657f5b710a73d93
Ozioma <oziomaokoroafor@gmail.com>
2022-05-19 13:01:01 +0100

    Add amazon challenge

--- src/algoexpertProblems/Amazon.java
+++ src/algoexpertProblems/Amazon.java
@@ -1,2 +1,25 @@
-package algoexpertProblems;public class Amazon {
+package algoexpertProblems;
+
+public class Amazon {
+
 }
+//    The virtual private server on Amazon's Web Service cloud is used to run programs that help perform calculations on large data sets. Recently, the performance of some programs has degraded.
+//
+//
+//
+//        Technical support has recommended that processes that consume a lot of main memory should be deleted. Unfortunately, the command-line shell that you use only lets you delete processes that form a single contiguous segment of a given fixed size. The size of a contiguous segment is the number of contiguous processes in the main memory.
+//
+//
+//
+//        Find the minimum amount of main memory used by all of your processes in your instance after you delete a contiguous segment of processes.
+//
+//
+//        Example
+//
+//        processes = [10, 4, 8, 13, 20]
+//
+//        m = 2
+//
+//
+//
+//        The processes in main memory are visualized below:
